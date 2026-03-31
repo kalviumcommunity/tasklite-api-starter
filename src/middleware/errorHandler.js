9784eee8cee4953a84ca22cf76673b002a03fa9e
varnikag24 <143873214+varnikag24@users.noreply.github.com>
2026-03-31 14:00:56 +0530

    Create errorHandler.js

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,10 @@
+// TODO: Implement centralized error handler
+// - Log error
+// - Return JSON:
+// { success: false, message: "Internal Server Error" }
+
+const errorHandler = (err, req, res, next) => {
+  // TODO
+};
+
+module.exports = errorHandler;
